68f26ab4aa932b63fb933427bc7ee4b2e762066d
Alvaro Contreras <alvaro9210@gmail.com>
2021-11-07 20:52:17 -0800

    Add IUserDTO to create new users

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,7 +1,13 @@
-export default interface IUser {
+export interface IUser {
     id: number;
     login: string;
     password: string;
     age: number;
     isDeleted: boolean;
+}
+
+export interface IUserDTO {
+    login: string;
+    password: string;
+    age: number;
 }
