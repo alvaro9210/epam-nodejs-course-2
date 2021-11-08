8dea3e04ffdab3ee175ea7e3cae2e67fbe580f78
Alvaro Contreras <alvaro9210@gmail.com>
2021-11-07 20:51:43 -0800

    Update SQL command to change isDeleted column name

--- src/users.sql
+++ src/users.sql
@@ -2,11 +2,11 @@
 -- Users table creation
 -- ====================
 CREATE TABLE users (
-    id int,
+    id SERIAL PRIMARY KEY,
     login varchar(255),
     password varchar(255),
-    age int, 
-    isDeleted boolean
+    age int,
+    "isDeleted" boolean
 );
 
 -- ====================
@@ -14,8 +14,8 @@ CREATE TABLE users (
 -- ====================
 INSERT INTO users 
 VALUES 
-    ( 1, 'user1', 'user1password', 21, false ),
-    ( 2, 'user2', 'user2password', 22, false ),
-    ( 3, 'user3', 'user3password', 23, false ),
-    ( 4, 'user4', 'user4password', 24, false ),
-    ( 5, 'user5', 'user5password', 25, false );
+    ( DEFAULT, 'user1', 'user1password', 21, false ),
+    ( DEFAULT, 'user2', 'user2password', 22, false ),
+    ( DEFAULT, 'user3', 'user3password', 23, false ),
+    ( DEFAULT, 'user4', 'user4password', 24, false ),
+    ( DEFAULT, 'user5', 'user5password', 25, false );
